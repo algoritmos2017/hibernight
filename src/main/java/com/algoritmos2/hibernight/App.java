a2f75f7cd9e3c98995aaeaa7dfdd589dfe9da9d2
Diego Anazonian <diego.anazonian@almundo.com>
2017-03-30 09:56:19 -0300

    first commit

---
+++ src/main/java/com/algoritmos2/hibernight/App.java
@@ -0,0 +1,13 @@
+package com.algoritmos2.hibernight;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
