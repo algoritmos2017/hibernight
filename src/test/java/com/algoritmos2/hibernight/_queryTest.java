2f87cb553e47b6ac990a5befa6e505b4b94c3bb9
Ezequiel <ezequiel_luppino@live.com>
2017-06-30 18:29:37 -0300

    Arreglado el where para que soporte un camino largo

--- src/test/java/com/algoritmos2/hibernight/_queryTest.java
+++ src/test/java/com/algoritmos2/hibernight/_queryTest.java
@@ -1,9 +1,8 @@
 package com.algoritmos2.hibernight;
 
 import com.algoritmos2.hibernight.repository.Query;
-import com.algoritmos2.hibernight.model.Direccion;
-import com.algoritmos2.hibernight.model.Persona;
-import com.algoritmos2.hibernight.model.annotations.Table;
+import com.algoritmos2.hibernight.model.profeModel.*;
+import com.algoritmos2.hibernight.model.QueryBuilder;
 import com.algoritmos2.hibernight.model.mapper.Mapper;
 
 import org.junit.After;
@@ -19,42 +18,60 @@ import java.util.Set;
 
 public class _queryTest {
 	String xql = "";
-	String sqlEsperado = "SELECT persona.id_persona, persona.nombre, persona.id_direccion, direccion.id_direccion, direccion.calle, direccion.numero, persona.id_ocupacion, ocupacion.id_ocupacion, ocupacion.descripcion, ocupacion.id_tipoocupacion, tipo_ocupacion.id_tipoocupacion, tipo_ocupacion.descripcion  FROM persona INNER JOIN direccion ON persona.id_direccion=direccion.id_direccion  INNER JOIN ocupacion ON persona.id_ocupacion=ocupacion.id_ocupacion  INNER JOIN tipo_ocupacion ON ocupacion.id_tipoocupacion=tipo_ocupacion.id_tipoocupacion  WHERE nombre='Juan' AND direccion.calle=2 ";
+	String sqlEsperado = "SELECT persona.id_ocupacion, ocupacion.id_tipoocupacion, tipo_ocupacion.id_tipoocupacion, tipo_ocupacion.descripcion, ocupacion.id_ocupacion, ocupacion.descripcion, persona.id_persona, persona.nombre  FROM persona INNER JOIN ocupacion ON persona.id_ocupacion=ocupacion.id_ocupacion  INNER JOIN tipo_ocupacion ON ocupacion.id_tipoocupacion=tipo_ocupacion.id_tipoocupacion  WHERE persona.nombre='Juan' ";
+
+	/*
+	 * @Test public void _queryConArgumentos(){ xql =
+	 * "$nombre=? and $direccion.calle=?"; String sqlDeLaFuncion =
+	 * Query._query(Persona.class, xql, "Juan", 2);
+	 * 
+	 * System.out.println(sqlDeLaFuncion);
+	 * 
+	 * Assert.assertEquals(sqlDeLaFuncion, sqlEsperado); }
+	 */
 	
 	@Test
-	public void _queryConArgumentos(){
-		xql = "$nombre=? and $direccion.calle=?";
-		String sqlDeLaFuncion = Query._query(Persona.class, xql, "Juan", 2);
-		
+	public void _querySinSignosDePregunta() {
+		xql = "$nombre=Juan";
+		String sqlDeLaFuncion = Query._query(Persona.class, xql);
+
 		System.out.println(sqlDeLaFuncion);
-		//System.out.println(Mapper.traducirDeObjetosARelacional(Persona.class));
-		
 		Assert.assertEquals(sqlDeLaFuncion, sqlEsperado);
 	}
+	
 	@Test
-	public void _querySinArgumentos(){
-		xql = "$nombre=Juan and $direccion.calle=2";
+	public void xqlConMuchosPuntos() {
+		xql = "$ocupacion.tipoOcupacion.descripcion like ?";
 		String sqlDeLaFuncion = Query._query(Persona.class, xql);
-		
+		sqlEsperado = "SELECT persona.id_ocupacion, ocupacion.id_tipoocupacion, tipo_ocupacion.id_tipoocupacion, tipo_ocupacion.descripcion, ocupacion.id_ocupacion, ocupacion.descripcion, persona.id_persona, persona.nombre  FROM persona INNER JOIN ocupacion ON persona.id_ocupacion=ocupacion.id_ocupacion  INNER JOIN tipo_ocupacion ON ocupacion.id_tipoocupacion=tipo_ocupacion.id_tipoocupacion  WHERE tipo_ocupacion.descripcion LIKE ? ";
+
 		//System.out.println(sqlDeLaFuncion);
 		Assert.assertEquals(sqlDeLaFuncion, sqlEsperado);
 	}
+	
 	@Test
-	public void xqlConCaracteresInvalidosSeIgnoran(){
-		xql = "$no(mbre=Jua)n an&d $direccion.call%e=2";
-		String sqlDeLaFuncion = Query._query(Persona.class, xql);
+	public void where() {
+		QueryBuilder queryBuilder = new QueryBuilder();
+		String xql = "$ocupacion.tipoOcupacion.descripcion like ?";
+		Mapper.obternerWhere(Persona.class, xql, queryBuilder);
 		
-		//System.out.println(sqlDeLaFuncion);
-		Assert.assertEquals(sqlDeLaFuncion, sqlEsperado);
+		//System.out.println(queryBuilder.getWhere());
+		Assert.assertEquals("tipo_ocupacion.descripcion LIKE ? ", queryBuilder.getWhere());
 	}
+
 	@Test(expected = Error.class)
-	public void _queryCantidadMenorDeArgumentosDaError(){
-		xql = "$nombre=? and $direccion.calle=2";
+	public void xqlConCaracteresInvalidosDaError() {
+		xql = "$no(mbre=Jua)n an&d $direccion.call%e=2";
 		Query._query(Persona.class, xql);
 	}
-	@Test(expected = Error.class)
-	public void _queryCantidadMayorDeArgumentosDaError(){
-		xql = "$no(mbre=Jua)n an&d $direccion.call%e=2";
-		Query._query(Persona.class, xql, "Juan", 2,3,5,6,7,2,"afs");
-	}
+	/*
+	 * @Test(expected = Error.class) public void
+	 * _queryCantidadMenorDeArgumentosDaError(){ xql =
+	 * "$nombre=? and $direccion.calle=2"; Query._query(Persona.class, xql); }
+	 * 
+	 * @Test(expected = Error.class) public void
+	 * _queryCantidadMayorDeArgumentosDaError(){ xql =
+	 * "$no(mbre=Jua)n an&d $direccion.call%e=2"; Query._query(Persona.class,
+	 * xql, "Juan", 2,3,5,6,7,2,"afs"); }
+	 */
 }
